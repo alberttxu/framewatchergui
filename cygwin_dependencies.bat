1a8878849a048176bb67b3bdb6d8ba7769a8e4e5
Albert Xu <albert.t.xu@gmail.com>
2019-08-16 15:02:50 -0400

    add cygwin_dependencies.bat

---
+++ cygwin_dependencies.bat
@@ -0,0 +1,9 @@
+setup-x86_64.exe ^
+--arch x86_64 ^
+--root C:\cygwin64 ^
+--site http://cygwin.mirror.constant.com ^
+--packages ^
+python2-devel,^
+python3-devel,^
+python36-tkinter,^
+python36-pip
